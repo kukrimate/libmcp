f4e9bb22a58fb9317bd411ab87dd2f5a092f3027
Máté Kukri <kukri.mate@gmail.com>
2018-12-18 16:53:54 +0100

    Add factory default settings to config.h

--- config.h
+++ config.h
@@ -5,31 +5,31 @@
 // SPI settings
 mcp2210_spi_settings_t config_spi_settings =
 {
-	.bitrate               = b32(1464), /* this is the minimum bitrate */
-	.idle_cs               = b16(0x1ff),
-	.active_cs             = b16(0),
-	.cs_to_data_delay      = b16(100),
-	.data_to_cs_delay      = b16(100),
-	.data_delay            = b16(100),
-	.bytes_per_transaction = b16(64),
+	.bitrate               = b32(1000000),
+	.idle_cs               = b16(0),
+	.active_cs             = b16(0x1ff),
+	.cs_to_data_delay      = b16(0),
+	.data_to_cs_delay      = b16(0),
+	.data_delay            = b16(0),
+	.bytes_per_transaction = b16(4),
 	.spi_mode              = 0
 };
 
 // Chip settings
 mcp2210_chip_settings_t config_chip_settings =
 {
-	.pins = { MCP2210_PIN_CS,
-			MCP2210_PIN_GPIO,
-			MCP2210_PIN_GPIO,
-			MCP2210_PIN_GPIO,
-			MCP2210_PIN_GPIO,
-			MCP2210_PIN_GPIO,
-			MCP2210_PIN_GPIO,
+	.pins = { MCP2210_PIN_GPIO,
+			MCP2210_PIN_CS,
+			MCP2210_PIN_DEDICATED,
+			MCP2210_PIN_DEDICATED,
+			MCP2210_PIN_DEDICATED,
+			MCP2210_PIN_DEDICATED,
+			MCP2210_PIN_DEDICATED,
 			MCP2210_PIN_GPIO,
 			MCP2210_PIN_GPIO },
-	.gpio_default   = b16(0),
-	.gpio_direction = b16(0),
-	.other_settings = 0,
+	.gpio_default   = b16(0x1ff),
+	.gpio_direction = b16(0x1ff),
+	.other_settings = 0x10,
 	.nvram_lock     = 0,
 	.new_password   = { 0, 0, 0, 0, 0, 0, 0, 0 }
 };
@@ -42,14 +42,12 @@ mcp2210_key_parameters_t config_key_parameters =
 	//.power_options = SELF_POWERED,
 	.power_options  = HOST_POWERED,
 	// Required current in 2 mA
-	.current_amount = 250 // default is 100 mA
+	.current_amount = 50
 };
 
 // Product name
-//#define CONFIG_PRODUCT_NAME "MCP2210 USB to SPI Master"
-#define CONFIG_PRODUCT_NAME "USB SPI Flasher"
+#define CONFIG_PRODUCT_NAME "MCP2210 USB to SPI Master"
 // Manufacturer name
-//#define CONFIG_MANUFACTURER_NAME "Microchip Technology Inc."
-#define CONFIG_MANUFACTURER_NAME "Mate Kukri"
+#define CONFIG_MANUFACTURER_NAME "Microchip Technology Inc."
 
 #endif
--- config_device.sh
+++ config_device.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Shell script to easily configure a new device
+
+# Change NVRAM (power-up default) settings 
 ./mcp2210_conf set 1 -n spi_settings
 ./mcp2210_conf set 1 -n chip_settings
+
+# Change the settings in RAM too to avoid re-plugging the device
 ./mcp2210_conf set 1 spi_settings
 ./mcp2210_conf set 1 chip_settings
-./mcp2210_conf set 1 -n key_parameters
-./mcp2210_conf set 1 -n product_name
-./mcp2210_conf set 1 -n manufacturer_name
+
+# Changing these is usually not required
+#./mcp2210_conf set 1 -n key_parameters
+#./mcp2210_conf set 1 -n product_name
+#./mcp2210_conf set 1 -n manufacturer_name
